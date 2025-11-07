695e5f8c444cca8e52fc3c3b870cbf616acb7b9f
mathisdesraches <mathis.desraches@edu.univ-fcomte.fr>
2025-11-07 14:01:06 +0100

    js

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/LLH4HAdn)
-- nom :
-- prénom :
-- URL Netlify :
+- nom : DESRACHES
+- prénom : Mathis
+- URL Netlify : https://nimble-pixie-5d4921.netlify.app/
 
 # Travail
 
--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
 // TODO
+
+const paragraphe = document.querySelector("#a-mettre-en-rouge");
+paragraphe.style.color = "red";
+
+
+const bouton = document.querySelector("#en-rouge-suite-a-click");
+bouton.addEventListener("click", () => {
+  bouton.style.color = "red";
+});
+
+
+const tousLesH2 = document.querySelectorAll("h2");
+
+tousLesH2.forEach((elem) => {
+  elem.addEventListener("click", (evt) => {
+    evt.target.style.color = "red";
+  });
+});
